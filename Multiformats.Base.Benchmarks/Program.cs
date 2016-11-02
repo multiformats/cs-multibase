3260d2a4e9a6cff8253c8f141343ddbacfe2de62
Trond Bråthen <tabrath@gmail.com>
2016-11-02 22:51:18 +0100

    added benchmarks

---
+++ Multiformats.Base.Benchmarks/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace Multiformats.Base.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new BenchmarkSwitcher(typeof(Program).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+}
