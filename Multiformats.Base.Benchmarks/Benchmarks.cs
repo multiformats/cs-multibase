14e9ec2a534bee1e1f854b34d3ce75aea78b9c9f
Trond Bråthen <tabrath@gmail.com>
2016-11-03 14:06:34 +0100

    implemented benchmarks and improved base16 and base 58 performance

--- Multiformats.Base.Benchmarks/Benchmarks.cs
+++ Multiformats.Base.Benchmarks/Benchmarks.cs
@@ -1,78 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Attributes.Columns;
-using BenchmarkDotNet.Attributes.Jobs;
+﻿using BenchmarkDotNet.Attributes.Jobs;
 
 namespace Multiformats.Base.Benchmarks
 {
-    [LegacyJitX86Job, LegacyJitX64Job, RyuJitX64Job, AllStatisticsColumn]
-    public class Benchmarks
-    {
-        private byte[] _bytes;
-        private Dictionary<MultibaseEncoding, string> _encoded;
+    [LegacyJitX86Job, RyuJitX64Job]
+    public class BenchmarkBase2 : BenchmarkBase<Base2Encoding> { }
 
-        [Setup]
-        public void Setup()
-        {
-            _bytes = new byte[64];
-            new Random(Environment.TickCount).NextBytes(_bytes);
+    [LegacyJitX86Job, RyuJitX64Job]
+    public class BenchmarkBase8 : BenchmarkBase<Base8Encoding> { }
 
-            _encoded = new Dictionary<MultibaseEncoding, string>
-            {
-                {Multibase.Base2, Multibase.Base2.Encode(_bytes)},
-                {Multibase.Base8, Multibase.Base8.Encode(_bytes)},
-                {Multibase.Base10, Multibase.Base10.Encode(_bytes)},
-                {Multibase.Base16, Multibase.Base16.Encode(_bytes)},
-                {Multibase.Base32, Multibase.Base32.Encode(_bytes)},
-                {Multibase.Base58, Multibase.Base58.Encode(_bytes)},
-                {Multibase.Base64, Multibase.Base64.Encode(_bytes)}
-            };
-        }
+    [LegacyJitX86Job, RyuJitX64Job]
+    public class BenchmarkBase10 : BenchmarkBase<Base10Encoding> { }
 
-        [Benchmark]
-        public string Encode_Base2() => Multibase.Base2.Encode(_bytes);
+    [LegacyJitX86Job, RyuJitX64Job]
+    public class BenchmarkBase16 : BenchmarkBase<Base16Encoding> { }
 
-        [Benchmark]
-        public byte[] Decode_Base2() => Multibase.Decode(_encoded[Multibase.Base2]);
+    [LegacyJitX86Job, RyuJitX64Job]
+    public class BenchmarkBase32 : BenchmarkBase<Base32Encoding> { }
 
-        [Benchmark]
-        public string Encode_Base8() => Multibase.Base8.Encode(_bytes);
+    [LegacyJitX86Job, RyuJitX64Job]
+    public class BenchmarkBase58 : BenchmarkBase<Base58Encoding> { }
 
-        [Benchmark]
-        public byte[] Decode_Base8() => Multibase.Decode(_encoded[Multibase.Base8]);
-
-        [Benchmark]
-        public string Encode_Base10() => Multibase.Base10.Encode(_bytes);
-
-        [Benchmark]
-        public byte[] Decode_Base10() => Multibase.Decode(_encoded[Multibase.Base10]);
-
-        [Benchmark]
-        public string Encode_Base16() => Multibase.Base16.Encode(_bytes);
-
-        [Benchmark]
-        public byte[] Decode_Base16() => Multibase.Decode(_encoded[Multibase.Base16]);
-
-        [Benchmark]
-        public string Encode_Base32() => Multibase.Base32.Encode(_bytes);
-
-        [Benchmark]
-        public byte[] Decode_Base32() => Multibase.Decode(_encoded[Multibase.Base32]);
-
-        [Benchmark]
-        public string Encode_Base58() => Multibase.Base58.Encode(_bytes);
-
-        [Benchmark]
-        public byte[] Decode_Base58() => Multibase.Decode(_encoded[Multibase.Base58]);
-
-        [Benchmark]
-        public string Encode_Base64() => Multibase.Base64.Encode(_bytes);
-
-        [Benchmark]
-        public byte[] Decode_Base64() => Multibase.Decode(_encoded[Multibase.Base64]);
-    }
+    [LegacyJitX86Job, RyuJitX64Job]
+    public class BenchmarkBase64 : BenchmarkBase<Base64Encoding> { }
 }
--- Multiformats.Base/Base58Encoding.cs
+++ Multiformats.Base/Base58Encoding.cs
@@ -1,4 +1,13 @@
 using System;
+using System.CodeDom.Compiler;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading;
 using SimpleBase;
 
 namespace Multiformats.Base
@@ -10,7 +19,13 @@ namespace Multiformats.Base
 
         public override string Encode(byte[] data) => Encode(data, Base58Alphabet.Bitcoin);
 
-        public string Encode(byte[] data, Base58Alphabet alphabet) => (alphabet == Base58Alphabet.Bitcoin ? 'z' : 'Z') + (alphabet == Base58Alphabet.Bitcoin ? Base58.Bitcoin.Encode(data) : Base58.Flickr.Encode(data));
+        public string Encode(byte[] data, Base58Alphabet alphabet)
+        {
+            return (alphabet == Base58Alphabet.Bitcoin ? 'z' : 'Z') +
+                   (alphabet == Base58Alphabet.Bitcoin
+                       ? EncodeAlphabet(data, BitcoinAlphabet)
+                       : EncodeAlphabet(data, FlickrAlphabet));
+        }
 
         public override byte[] Decode(string str)
         {
@@ -20,12 +35,77 @@ namespace Multiformats.Base
             switch (id)
             {
                 case 'Z':
-                    return Base58.Flickr.Decode(str);
+                    return DecodeAlphabet(str, FlickrAlphabet);
                 case 'z':
-                    return Base58.Bitcoin.Decode(str);
+                    return DecodeAlphabet(str, BitcoinAlphabet);
                 default:
                     throw new NotSupportedException($"Unsupported identifier: {id}");
             }
+        }
+
+        private const string BitcoinAlphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        private const string FlickrAlphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
+
+        private static string EncodeAlphabet(byte[] b, string alphabet)
+        {
+            return
+                new string(
+                    b.TakeWhile(c => c == 0)
+                        .Select(_ => alphabet[0])
+                        .Concat(ParseBigInt(b.Aggregate<byte, BigInteger>(0, (current, t) => current * 256 + t),
+                            alphabet))
+                        .Reverse()
+                        .ToArray());
+        }
+
+        private static IEnumerable<char> ParseBigInt(BigInteger intData, string alphabet)
+        {
+            var len = alphabet.Length;
+            while (intData > 0)
+            {
+                var rem = (int)(intData % len);
+                intData /= len;
+                yield return alphabet[rem];
+            }
+        }
+
+        private static Dictionary<string, byte[]> _decodeMap;
+
+        private static byte[] CreateDecodeMap(string alphabet)
+        {
+            var map = Enumerable.Range(0, 256).Select(b => (byte)0xFF).ToArray();
+            for (var i = 0; i < alphabet.Length; i++)
+                map[alphabet[i]] = (byte)i;
+            return map;
+        }
+
+        private static byte[] GetDecodeMap(string alphabet)
+        {
+            if (_decodeMap == null)
+                _decodeMap = new Dictionary<string, byte[]>();
+
+            byte[] map;
+            if (_decodeMap.TryGetValue(alphabet, out map))
+                return map;
+
+            map = CreateDecodeMap(alphabet);
+            _decodeMap.Add(alphabet, map);
+            return map;
+        }
+
+        private static byte[] DecodeAlphabet(string b, string alphabet)
+        {
+            var decodeMap = GetDecodeMap(alphabet);
+            var len = alphabet.Length;
+
+            return b.TakeWhile(c => c == alphabet[0])
+                .Select(_ => (byte) 0)
+                .Concat(b.Select(c => decodeMap[c])
+                    .Aggregate<byte, BigInteger>(0, (current, c) => current * len + c)
+                    .ToByteArray()
+                    .Reverse()
+                    .SkipWhile(c => c == 0))
+                .ToArray();
         }
     }
 
